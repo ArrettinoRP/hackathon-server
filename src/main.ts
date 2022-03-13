37620a4f3aea894fc160de8a6e1ca0b6cfc1f08c
arrettino <pabloarrettino@gmail.com>
2022-03-13 13:44:17 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { initDatabase } from './database';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { logger: ['debug'] });
+  const app = await NestFactory.create(AppModule, { logger: ['debug'], cors: true });
   initDatabase();
   await app.listen(5000);
 }
